0afed84dff46e516501800e7dbb78a1044a5ece9
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2018-05-28 10:40:53 -0300

    WIP implemented secondary index merging, needs testing

--- hw1/mergefiles.c
+++ hw1/mergefiles.c
@@ -3,6 +3,92 @@
 #include <string.h>
 #include "datatypes.c"
 
+typedef struct courselistelem {
+    char course[3];
+    struct courselistelem* next;
+} CourseListElem;
+
+typedef struct courselist {
+    CourseListElem* first;
+    CourseListElem* last;
+    unsigned int size;
+} CourseList;
+
+CourseList* CourseListCreate()
+{
+   CourseList* l = (CourseList*)malloc(sizeof(CourseList));
+   l->first = NULL;
+   l->last = NULL;
+   return l;
+}
+
+void appendToCourseList(char* course, CourseList* l)
+{
+    CourseListElem* newelem = (CourseListElem*)malloc(sizeof(CourseListElem));
+    strcpy(newelem->course, course);
+    newelem->next = l->first;
+    l->first = newelem;
+    if(l->last == NULL){
+        l->last = newelem;
+    }
+    ++(l->size);
+}
+
+int isInList(char* course, CourseList* l)
+{
+    CourseListElem* elem = l->first;
+    while(elem != NULL)
+    {
+        if(strcmp(elem->course, course) == 0)
+        {
+            return 1;
+        }
+        elem = elem->next;
+    }
+    return 0;
+}
+
+void heapsort_sec_idx(Secidx* secidxvec, int ncourses)
+{
+    int i = ncourses/2;
+    int parentidx, childidx;
+    Secidx tmpreg;
+    while(1)
+    {
+        if (i > 0) {
+            --i;
+            tmpreg = secidxvec[i];
+        } else {
+            --ncourses;
+            if(!ncourses)
+            {
+                return;
+            }
+            tmpreg = secidxvec[ncourses];
+            secidxvec[ncourses] = secidxvec[0];
+        }
+        
+        parentidx = i;
+        childidx = 2*i + 1;
+        while(childidx < ncourses)
+        {
+            if((childidx + 1 < ncourses) && (strcmp(secidxvec[childidx + 1].course, secidxvec[childidx].course) > 0))
+            {
+                ++childidx;
+            }
+            if(strcmp(secidxvec[childidx].course, tmpreg.course) > 0)
+            {
+                secidxvec[parentidx] = secidxvec[childidx];
+                parentidx = childidx;
+                childidx = 2*parentidx + 1;
+            } else {
+                break;
+            }
+        }
+        secidxvec[parentidx] = tmpreg;
+    }
+}
+
 char datafile0[31];
 char primidxfile0[40];
 char secidxfile0[40];
@@ -30,6 +116,28 @@ void gen_idx_fname(char* outputfname, char* inputfname, char ext[])
     char* dotptr = strrchr(outputfname, '.');
     *dotptr = '\0';
     strcat(outputfname, ext);
+}
+
+void gen_primkey(char primkey[], Reg reg)
+{
+    strcpy(primkey, reg.matric);
+    char* srcptr = reg.name;
+    int dstidx = 6;
+    while(*srcptr && dstidx < 30)
+    {
+        if(*srcptr != ' ')
+        {
+            primkey[dstidx] = toupper(*srcptr);
+            ++dstidx;
+        }
+        ++srcptr;
+    }
+    while(dstidx < 30)
+    {
+        primkey[dstidx] = ' ';
+        ++dstidx;
+    }
+    primkey[dstidx] = '\0';
 }
 
 unsigned long int fsize(char* fname)
@@ -88,6 +196,8 @@ int main(int argc, char* argv[])
     fread(&primidx0, sizeof(Primidx), 1, primidxfp0);
     fread(&primidx1, sizeof(Primidx), 1, primidxfp1);
 
+    CourseList* courselist = CourseListCreate();
+
     int dataoutidx = 0;
     while(!feof(primidxfp0) || !feof(primidxfp1))
     {
@@ -107,6 +217,10 @@ int main(int argc, char* argv[])
                 primidx1.rrn = dataoutidx;
                 dataoutidx++;
                 fread(&tmpreg, sizeof(Reg), 1, datafp1);
+                if(!isInList(tmpreg.course, courselist))
+                {
+                    appendToCourseList(tmpreg.course, courselist);
+                }
                 fwrite(&tmpreg, sizeof(Reg), 1, datafpout);
                 fwrite(&primidx1, sizeof(Primidx), 1, primidxfpout);
                 fread(&primidx1, sizeof(Primidx), 1, primidxfp1);
@@ -118,6 +232,10 @@ int main(int argc, char* argv[])
                 primidx0.rrn = dataoutidx;
                 dataoutidx++;
                 fread(&tmpreg, sizeof(Reg), 1, datafp0);
+                if(!isInList(tmpreg.course, courselist))
+                {
+                    appendToCourseList(tmpreg.course, courselist);
+                }
                 fwrite(&tmpreg, sizeof(Reg), 1, datafpout);
                 fwrite(&primidx0, sizeof(Primidx), 1, primidxfpout);
                 fread(&primidx0, sizeof(Primidx), 1, primidxfp0);
@@ -127,6 +245,10 @@ int main(int argc, char* argv[])
             primidx0.rrn = dataoutidx;
             dataoutidx++;
             fread(&tmpreg, sizeof(Reg), 1, datafp0);
+            if(!isInList(tmpreg.course, courselist))
+            {
+                appendToCourseList(tmpreg.course, courselist);
+            }
             fwrite(&tmpreg, sizeof(Reg), 1, datafpout);
             fwrite(&primidx0, sizeof(Primidx), 1, primidxfpout);
             fread(&primidx0, sizeof(Primidx), 1, primidxfp0);
@@ -135,11 +257,72 @@ int main(int argc, char* argv[])
             primidx1.rrn = dataoutidx;
             dataoutidx++;
             fread(&tmpreg, sizeof(Reg), 1, datafp1);
+            if(!isInList(tmpreg.course, courselist))
+            {
+                appendToCourseList(tmpreg.course, courselist);
+            }
             fwrite(&tmpreg, sizeof(Reg), 1, datafpout);
             fwrite(&primidx1, sizeof(Primidx), 1, primidxfpout);
             fread(&primidx1, sizeof(Primidx), 1, primidxfp1);
         }
     }
+
+    /*Merging secondary index*/
+    fclose(datafpout);
+    unsigned long outsize = fsize(datafileout)/sizeof(Reg);
+    Secidx* secidxvec = (Secidx*)malloc((courselist->size)*sizeof(Secidx));
+    
+    int i = 0;
+    CourseListElem* elem;
+    elem = courselist->first;
+    while(elem != NULL)
+    {
+        strcpy(secidxvec[i].course, elem->course);
+        secidxvec[i].head = -1;
+        elem = elem->next;
+        ++i;
+    }
+    heapsort_sec_idx(secidxvec, courselist->size);
+    secidxfpout = fopen(secidxfileout, "w");
+    fwrite(secidxvec, sizeof(Secidx), courselist->size, secidxfpout);
+    fclose(secidxfpout);
+
+    SecInvList* secinvlistvec = (SecInvList*)malloc(outsize*sizeof(SecInvList));
+    datafpout = fopen(datafileout, "r");
+    
+    int j, head;
+    i = 0;
+    while(i < outsize)
+    {
+        fread(&tmpreg, sizeof(Reg), 1, datafpout);
+        gen_primkey(secinvlistvec[i].primkey, tmpreg);
+        secinvlistvec[i].next = -1;
+        j = 0;
+        while(j < courselist->size)
+        {
+            if(strcmp(secidxvec[j].course, tmpreg.course) == 0)
+            {
+                head = secidxvec[j].head;
+                break;
+            }
+            ++j;
+        }
+        if(head == -1)
+        {
+            secidxvec[j].head = i;
+        } else {
+            while(secinvlistvec[head].next != -1)
+            {
+                head = secinvlistvec[head].next;
+            }
+            secinvlistvec[head].next = i;
+        }
+        ++i;
+    }
+
+    secinvlistfpout = fopen(secinvlistfileout, "w");
+    fwrite(secinvlistvec, sizeof(SecInvList), outsize, secinvlistfpout);
+    fclose(secinvlistfpout);
 
     fclose(primidxfp0);
     fclose(primidxfp1);
